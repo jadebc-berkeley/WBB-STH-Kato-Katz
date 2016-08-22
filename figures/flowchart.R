0912d2f2168dd08f4c37bc91ef7dbfae56e193ad
Jade Benjamin-Chung <jadebc@gmail.com>
2016-08-22 09:22:52 -0700

    changes to script to use new variables combining STH and main survey info

--- figures/flowchart.R
+++ figures/flowchart.R
@@ -27,15 +27,15 @@ kk=reord(kk)
 # Follow-up at year 2 
 # (compounds lost, moved, absent, withdrew, no LB, child death)
 # ---------------------------------------------
-dropout=table(wd$tr[wd$dropout==1])
-nolb=table(wd$tr[wd$nolb==1 & wd$dropout==1])
-withdrew=table(wd$tr[wd$withdrew==1 & wd$dropout==1])
-moved=table(wd$tr[wd$moved==1 & wd$dropout==1])
-cdeath=table(wd$tr[wd$cdeath==1 & wd$dropout==1])
-absent=table(wd$tr[wd$absent==1 & wd$dropout==1])
+dropout=table(wd$tr[wd$hhstatus!="E"])
+nolb=table(wd$tr[wd$sthnolb==1])
+withdrew=table(wd$tr[wd$sthwd==1])
+moved=table(wd$tr[wd$sthmoved==1])
+cdeath=table(wd$tr[wd$sthcd==1])
+absent=table(wd$tr[wd$sthabsent==1])
 
-compound_lost_j = rbind(dropout,moved,absent,withdrew,nolb,cdeath)
-rownames(compound_lost_j)=c("Compounds lost","Moved","Absent","Withdrew","No live birth","Child death")
+compound_lost_j = rbind(dropout,nolb,cdeath,moved,withdrew,absent)
+rownames(compound_lost_j)=c("Compounds lost","No live birth","Child death","Moved","Withdrew","Absent")
 
 # ---------------------------------------------
 # Compounds at year 2 in STH analysis
@@ -43,11 +43,6 @@ rownames(compound_lost_j)=c("Compounds lost","Moved","Absent","Withdrew","No liv
 sth.comp.tab=table(enr$dataid[enr$hhstatus=="E"],enr$tr[enr$hhstatus=="E"])
 sth.comp.tab[sth.comp.tab>1]=1
 CompoundsSTH=colSums(sth.comp.tab)
-
-# kk.comp.tab=table(kk$dataid,kk$tr)
-# kk.comp.tab[kk.comp.tab>1]=1
-# CompoundsSTH=colSums(kk.comp.tab)
-
 
 # ---------------------------------------------
 # Has sample T1, O1, C1
