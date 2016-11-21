77fc5e3cb28d1548a5d85626238723c20689b25e
Jade Benjamin-Chung <jadebc@gmail.com>
2016-11-21 15:04:21 -0800

    revised covariate list in adj

--- analysis/2a-PR-prev-adj.R
+++ analysis/2a-PR-prev-adj.R
@@ -18,9 +18,9 @@ d=preprocess.adj.sth(d)
 
 # roof and landphone excluded due to low prevalence
 
-W=c("counter","birthord","month","hfias","aged","sex","momage","momheight","momedu",
+W=c("counter","birthord","month","hfiacat","aged","sex","momage","momheight","momedu",
     "Nlt18","Ncomp","watmin","walls","floor",
-    "elec","asset_wardrobe","n_asset_table","n_asset_chair","asset_khat","asset_chouki",
+    "elec","asset_wardrobe","asset_table","asset_chair","asset_khat","asset_chouki",
     "asset_tv","asset_refrig","asset_bike","asset_moto","asset_sewmach","asset_mobile")
 
 dW=d[,c("block","tr","clusterid","sth","al","hw","tt",W)]
