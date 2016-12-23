74bdafbadda4c26b01a93fe938e4c5429208c695
Jade Benjamin-Chung <jadebc@gmail.com>
2016-12-23 11:47:06 -0800

    updated covs with binary to address sparse data

--- analysis/5-epg-effectmod/5j-PR-epg-adj-hmud.R
+++ analysis/5-epg-effectmod/5j-PR-epg-adj-hmud.R
@@ -28,10 +28,13 @@ d0=d[d$dirtfloor_hh==0,]
 # the following variables were dropped from the covariate list 
 # because they had many levels and one of the effect modification
 # strata has <20% prevalence: 
-# counter, month, birthorder, food security, number of individuals 
-# in compound, number of <18 individuals
+# counter, birthorder
+# using binary versions of the following due to sparse data: 
+# food security, number of individuals in compound, number of <18 individuals
+# age days, month
 # mobile phone dropped because of sparse data in one level 
-W=c("aged","sex","momage","momheight","momedu","watmin","walls","floor",
+W=c("wet","sac","sex","hfiacatbin","momagebin","momheightbin","momedu",
+    "Nlt18bin","Ncompbin","watminbin","walls","floor",
     "elec","asset_wardrobe","asset_table","asset_chair","asset_khat","asset_chouki",
     "asset_tv","asset_refrig","asset_bike","asset_moto","asset_sewmach")
 
