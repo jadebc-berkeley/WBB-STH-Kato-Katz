3c655112a621e5dda198c409bd4294508e6c2e33
Jade Benjamin-Chung <jadebc@gmail.com>
2017-01-30 09:18:20 -0800

    removed W from data frame

--- analysis/4-binary-effectmod/4eu-PR-prev-unadj-deftoday.R
+++ analysis/4-binary-effectmod/4eu-PR-prev-unadj-deftoday.R
@@ -25,19 +25,8 @@ d0=d[d$defday=="Yesterday" & d$defday!="",]
 
 # roof and landphone excluded due to low prevalence
 
-# the following variables were dropped from the covariate list 
-# because they had many levels and one of the effect modification
-# strata has <20% prevalence: counter, birthorder
-# using binary versions of the following due to sparse data: 
-# food security, number of individuals in compound, number of <18 individuals
-# age days, month
-W=c("wet","sac","sex","hfiacatbin","momagebin","momheightbin","momedu",
-    "Nlt18bin","Ncompbin","watminbin","walls","floor",
-    "elec","asset_wardrobe","asset_table","asset_chair","asset_khat","asset_chouki",
-    "asset_tv","asset_refrig","asset_bike","asset_moto","asset_sewmach","asset_mobile")
-
-dW1=d1[,c("block","tr","clusterid","sth","al","hw","tt",W)]
-dW0=d0[,c("block","tr","clusterid","sth","al","hw","tt",W)]
+dW1=d1[,c("block","tr","clusterid","sth","al","hw","tt")]
+dW0=d0[,c("block","tr","clusterid","sth","al","hw","tt")]
 
 #----------------------------------------------
 # H1: Unadjusted prevalence ratios; each arm vs. 
