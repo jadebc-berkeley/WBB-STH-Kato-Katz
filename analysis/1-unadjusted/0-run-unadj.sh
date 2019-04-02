0424cbdc41cd7bc532cd512fe3c8c29556d63057
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-02 15:37:34 -0700

    add missing scripts

--- analysis/1-unadjusted/0-run-unadj.sh
+++ analysis/1-unadjusted/0-run-unadj.sh
@@ -1,20 +1,16 @@
 
 #!/bin/bash
 
-cd
-rm -f "~/Box Sync/WASHB Parasites/Results/Jade/sth_prev.RData"
-rm -f "~/Box Sync/WASHB Parasites/Results/Jade/sth_pr_unadj.RData"
-rm -f "~/Box Sync/WASHB Parasites/Results/Jade/sth_mh_pr_unadj.RData"
-rm -f "~/Box Sync/WASHB Parasites/Results/Jade/sth_pr_epg_unadj.RData"
-
-cd "Documents/CRG/wash-benefits/bangladesh/src/sth/analysis/1-unadjusted/"
-
 rm -f 1a-prev.Rout
 rm -f 1b-PR-prev-unadj.Rout
 rm -f 1c-PR-prev-mh-unadj.Rout
 rm -f 1d-PR-epg-unadj.Rout
+rm -f 1e-PR-prev-unadj-tmle.Rout
+rm -f 1f-PR-prev-mh-unadj-tmle.Rout
 
 R CMD BATCH 1a-prev.R 
 R CMD BATCH 1b-PR-prev-unadj.R 
 R CMD BATCH 1c-PR-prev-mh-unadj.R
 R CMD BATCH 1d-PR-epg-unadj.R
+R CMD BATCH 1e-PR-prev-unadj-tmle.R
+R CMD BATCH 1f-PR-prev-mh-unadj-tmle.R
