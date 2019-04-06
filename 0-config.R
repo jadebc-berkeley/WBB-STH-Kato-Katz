56e277edc802329bf03fb22f16a8db4e2cfb8a55
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-06 10:26:07 -0700

    add plot fns and pkgs

--- 0-config.R
+++ 0-config.R
@@ -11,6 +11,9 @@
 library(washb)
 library(here)
 library(devtools)
+library(ggplot2)
+library(grid)
+library(gridExtra)
 
 # define data paths
 # sth_data_path = "~/Dropbox/WASHB Parasites/Analysis datasets/Jade/sth.csv"
@@ -23,6 +26,7 @@ save_data_path = "~/Box Sync/WASHB Parasites/Results/Public-test/"
 # load base functions
 source(paste0(here::here(),"/analysis/0-base-programs.R"))
 source(paste0(here::here(),"/table-scripts/0-base-table-functions.R"))
+source(paste0(here::here(),"/figure-scripts/0-base-plot-functions.R"))
 
 # define figure and table paths
 figure_path = paste0(here::here(), "/figures/")
