65d9b410c75feeb52a9d9c9936b95a79880ab449
Jade Benjamin-Chung <jadebc@gmail.com>
2019-04-06 10:26:31 -0700

    update object names

--- table-scripts/make-table3.R
+++ table-scripts/make-table3.R
@@ -15,13 +15,27 @@ source(here::here("0-config.R"))
 #---------------------------------------------
 load(paste0(save_data_path,"sth_prev.RData"))
 load(paste0(save_data_path,"sth_prev_em.RData"))
-load(paste0(save_data_path,"sth_pr_adj_index.RData"))
+load(paste0(save_data_path,"sth_prev_unadj_tmle.RData"))
+load(paste0(save_data_path,"sth_pr_unadj_tmle.RData"))
+
+#---------------------------------------------
+# Subset to index children
+#---------------------------------------------
+load(paste0(save_data_path,"sth_pr_unadj_index.RData"))
+load(paste0(save_data_path, "sth_pr_epg_unadj_index.RData"))
 load(paste0(save_data_path,"sth_pr_epg_adj_index.RData"))
 load(paste0(save_data_path,"sth_prev_sub_unadj_index.RData"))
 load(paste0(save_data_path,"sth_prev_sub_unadj_ind.RData"))
-load(paste0(save_data_path,"sth_prev_unadj_tmle.RData"))
+
+#---------------------------------------------
+# Other children in index household 
+#---------------------------------------------
 load(paste0(save_data_path,"sth_em_ihh.RData"))
 load(paste0(save_data_path,"sth_prev_sub_unadj_ihh.RData"))
+
+# public subfolder missing these datasets: 
+load("~/Box Sync/WASHB Parasites/Results/Ayse/sth_em_ihh.RData")
+load("~/Box Sync/WASHB Parasites/Results/Ayse/sth_prev_sub_unadj_ihh.RData")
 
 # Change column name ihh objects
 colnames(al_em_prev_ihh0_a)=c("N","Prev","SD","Robust SE","lb","ub")
@@ -39,8 +53,8 @@ index.table=make.index.table.csv(al_prev,hw_prev,tt_prev,sth_prev,
                              al_em_prev_i1_j,hw_em_prev_i1_j,tt_em_prev_i1_j,sth_em_prev_i1_j, 
                              al_em_prev_ihh1_a,hw_em_prev_ihh1_a,tt_em_prev_ihh1_a,sth_em_prev_ihh1_a, 
                              al_em_prev_ihh0_a,hw_em_prev_ihh0_a,tt_em_prev_ihh0_a,sth_em_prev_ihh0_a, 
-                             al_rr_h1_unadj_tmle_a,hw_rr_h1_unadj_tmle_a,tt_rr_h1_unadj_tmle_a,sth_rr_h1_unadj_tmle_a,
-                             al_rr_h1_unadj_i1_a,hw_rr_h1_unadj_i1_a,tt_rr_h1_unadj_i1_a,sth_rr_h1_unadj_i1_a, 
+                             al_rr_h1_unadj_tmle_j,hw_rr_h1_unadj_tmle_j,tt_rr_h1_unadj_tmle_j,sth_rr_h1_unadj_tmle_j,
+                             al_rr_h1_unadj_i1_j,hw_rr_h1_unadj_i1_j,tt_rr_h1_unadj_i1_j,sth_rr_h1_unadj_i1_j, 
                              al_rr_h1_unadj_ihh1_a,hw_rr_h1_unadj_ihh1_a,tt_rr_h1_unadj_ihh1_a,sth_rr_h1_unadj_ihh1_a, 
                              al_rr_h1_unadj_ihh0_a,hw_rr_h1_unadj_ihh0_a,tt_rr_h1_unadj_ihh0_a,sth_rr_h1_unadj_ihh0_a,decimals=2)
 
